b59db13cdbd96330191f69d60d903b71547f56a4
AndreiListapp <andrei.carmici@listapp.eu>
2024-10-11 02:36:36 +0300

    modification

--- blue-bike/e2e/garages.test.js
+++ blue-bike/e2e/garages.test.js
@@ -1,30 +1,29 @@
 describe('GaragesScreen', () => {
-    beforeAll(async () => {
-      await device.launchApp();
-    });
-  
-    beforeEach(async () => {
-      await device.reloadReactNative();
-    });
-  
-    it('should show a spinner while loading and then render garages or "No garages available"', async () => {
-      await element(by.text('Garages')).tap();
-  
-      await expect(element(by.type('Spinner'))).toBeVisible();
-  
-      await waitFor(element(by.type('Spinner')))
-        .toBeNotVisible()
-        .withTimeout(5000);
-  
-      const garageCard = element(by.text('Getouw'));
-      const noGaragesText = element(by.text('No garages available'));
-  
-      await waitFor(garageCard)
-        .toBeVisible()
-        .withTimeout(2000)
-        .catch(async () => {
-          await expect(noGaragesText).toBeVisible();
-        });
-    });
+  beforeAll(async () => {
+    await device.launchApp();
   });
-  
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should show a spinner while loading and then render garages or "No garages available"', async () => {
+    await element(by.text('Garages')).tap();
+
+    await expect(element(by.type('Spinner'))).toBeVisible();
+
+    await waitFor(element(by.type('Spinner')))
+      .toBeNotVisible()
+      .withTimeout(5000);
+
+    const garageCard = element(by.text('Getouw'));
+    const noGaragesText = element(by.text('No garages available'));
+
+    await waitFor(garageCard)
+      .toBeVisible()
+      .withTimeout(2000)
+      .catch(async () => {
+        await expect(noGaragesText).toBeVisible();
+      });
+  });
+});
